d0820775b5fe95d22dabf44f99d9106e6781a92d
Robert Giessmann <rgiessmann@wlan-141-23-65-7.tubit.tu-berlin.de>
2016-03-24 13:34:27 +0100

    Merge remote-tracking branch 'origin/Ovicula-patch-1' into 0.1.0-functional-skeleton
    # Conflicts:
    #	footprint.py

--- footprint.py
+++ footprint.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+<<<<<<< HEAD
 #asdf
 import logging
 import sys
@@ -21,6 +22,21 @@ def main(argv=""):
            input_file = arg
     input_file += remaining_args
     ## TODO: check input_files; split   
+=======
+
+import logging                                
+import sys                                
+import getopt                                
+
+def main(argv=""):   
+  try:
+    opts, remaining_args = getopt.getopt(argv,"dhia:gf:o:k",["help","iterate","iterate-from=","iterate-to=","iter-step=","align-to=","down-pose","both-poses","generate-all-dummies","generate-dummies-from=","generate-dummies-to=","finegrain-step=","keep-dummies","dna-leading-strand=","dna-lagging-strand=","dna-leading-start=","dna-lagging-start=","bp-counting-parallel","minor-groove-offset=","output-prefix="])
+  except getopt.GetoptError:
+    print 'You provided unusual arguments. Call me with -h to learn more.'
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt in ('-h', '--help'):
+>>>>>>> origin/Ovicula-patch-1
 ...
 
 return
@@ -42,10 +58,20 @@ class Peak:
         return
     
 
+<<<<<<< HEAD
 def get_data(parameters):
     ## WARNING : this is a non-functional skeleton function
 
     ## TODO: read in the data from config and input files
+=======
+trace_list = ... #contains factor for each trace, footprint peaks, fractional occupancies, ligand and receptor concentrations, kd values
+peak_list = ... #contains Peaks and areas, new calculated areas
+
+def get_data(parameter):
+    #read csv (?) file
+    #if color blue and if peak size between ... and ..., copy size, name and area to peak_list
+return
+>>>>>>> origin/Ovicula-patch-1
 
     ## 1. create minimal data objects with classes
     trace_list = [
@@ -65,20 +91,56 @@ return deviance_for_all_peaks
 
 def determine_factor_numerically(ref, trace):
     # Robert's approach
+<<<<<<< HEAD
     optimal_factor = 1
 return optimal_factor
 
 def determine_factor_single_peak():
     optimal_factor=1
 return optimal_factor
+=======
+    #define reference
+    #loop1 begin
+     #for each trace
+     #loop2 begin
+       #use factors 0 to 3.5 in 0.01 steps , calculate new peak areas from peak areas
+       #use calculate_deviance_for_all_peaks with trace and ref
+       #list deviance_new (including factor)
+       #compare deviance_new with deviance_old, if better deviance_new -> deviance_old, else delete deviance_new
+     #end loop2
+     #put factor from deviance_old to trace_list (for right trace)
+    #end loop1
+  return optimal_factor
+
+def determine_factor_single_peak():
+    #define reference
+        #loop1 begin
+     #for each trace
+     #loop2 begin
+       #calculate factor for each Peak to bring to size of reference peak
+       #calculate new areas for every peak with factor 
+       #calculate deviance between each peak and reference
+       #list deviance_new (including factor)
+       #compare deviance_new with deviance_old, if better deviance_new -> deviance_old, else delete deviance_new
+     #end loop2
+     #put factor from deviance_old to trace_list (for right trace)
+    #end loop1
+return
+>>>>>>> origin/Ovicula-patch-1
 
 
 def correct_peaks_with_factor(trace, factor):
-    ...
+    #read and append peak_list
+    #multiply area of each peak for all traces with right factor from trace_list 
+    #add new area to peak_list
 return
 
 def which_peaks_differ(threshold=0.10):
-    ...
+    #begin loop
+      #for each peak 
+      #compare peak areas of traces with different concentration
+      #if difference >0.1 add peak to trace list
+    #end loop
 return peak_list
 
 def calculate_fractional_occupancies(peak_list):
